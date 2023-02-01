fc969aad7b5993f3200db23072dedbce7e142158
Miguel Machado <miiguellmp@gmail.com>
2023-02-01 13:59:56 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Share } from './components/Share'
 const bioData = {
   title: 'Olá 👋',
   content:
-    'Tenho 24 anos e trabalho como Frontend Engineer na Uber. Entusiasta de tecnologia e amante de jogos!',
+    'Tenho 24 anos e trabalho como Frontend Engineer na Globant. Entusiasta de tecnologia e amante de jogos!',
 }
 
 // TODO: Lib para SEO - Title, twitter card, etc...
